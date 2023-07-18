23f781ea06b29a1e29ac459be7276bfd73b45790
PiSethSeam <7piseth@gmail.com>
2023-07-18 14:55:11 +0700

    beautify layout

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -16,7 +16,7 @@ const TopBar = () => {
 
   return (
     <div className={`h-[60px] px-5 md:px-7 flex justify-between items-center`}>
-      <div className="absolute select-none left-[calc(50%-41px)]">
+      <div className="absolute select-none left-[calc(50%-41px)] z-10">
         <div className="relative w-20 bg-slate-300 dark:bg-slate-800 group rounded-full flex items-center cursor-pointer">
           <div
             className="w-1/2 h-full p-2 z-10"
@@ -46,20 +46,20 @@ const TopBar = () => {
           ></div>
         </div>
       </div>
-      <div className="absolute right-7 sm:left-[calc(53%)] top-[12px] flex place-items-center
-      rounded-lg text-white font-extrabold w-fit
-      bg-gradient-to-b from-sky-400 to-blue-800">
-        <GiTakeMyMoney size={35} className="p-1"/>
-        <p id="userMoney" className="rounded-r-[7px] min-w-[65px] text-center p-[6px] font-moul bg-[#95c2ee] text-black">១០០០៛</p>
-      </div>
-      <div className="absolute left-0 sm:left-[calc(39%)] top-[12px] flex place-items-center cursor-pointer active:scale-150 duration-300
+      <div className="absolute left-0 sm:left-[calc(50%-260px)] top-[12px] flex place-items-center cursor-pointer active:scale-150 duration-300
       rounded-lg text-white font-extrabold w-fit
       bg-gradient-to-b from-sky-400 to-blue-800"
-      onClick={()=>{
-        document.getElementById("userMoney").innerHTML = '១០០០៛'
-      }}>
-        <VscDebugRestart size={35} className="p-1"/>
-        <p id="userMoney" className="rounded-r-[7px] min-w-[65px] text-center p-[6px] bg-[#95c2ee] text-black font-extralight font-moul">លេងម្តងទៀត</p>
+        onClick={() => {
+          document.getElementById("userMoney").innerHTML = '១០០០៛'
+        }}>
+        <VscDebugRestart size={35} className="p-1" />
+        <p id="userMoney" className="max-[300px]:hidden rounded-r-[7px] min-w-[65px] text-center p-[6px] bg-[#95c2ee] text-black font-extralight font-moul">លេងម្តងទៀត</p>
+      </div>
+      <div className="absolute right-7 max-[400px]:right-0 sm:right-[calc(50%-190px)] top-[12px] flex place-items-center
+      rounded-lg text-white font-extrabold w-fit
+      bg-gradient-to-b from-sky-400 to-blue-800">
+        <GiTakeMyMoney size={35} className="p-1" />
+        <p id="userMoney" className="rounded-r-[7px] min-w-[65px] text-center p-[6px] font-moul bg-[#95c2ee] text-black">១០០០៛</p>
       </div>
     </div>
   )
